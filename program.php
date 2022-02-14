5d27e983da94444b9c65adbcb504a6fac2ef0f85
NayaRPL <naya.mmj@gmail.com>
2022-02-14 14:55:24 +0800

    day 65

---
+++ program.php
@@ -0,0 +1,22 @@
+<?php
+// program untuk mengetahui tanggal sekarang  
+    echo " Tanggal sekarang :";
+echo date('d-m-Y'); 
+echo "<br>";
+// di dalam membuat komentar atau keterangan program, kita dapat menggunakan  printah // , # atau /**/
+// tampilkan teks ke layar
+echo " tanggal sekarang:";
+# menampilkan tanggal 
+    echo date('d-m-Y');
+ /* ini adalah komentar panjang berbentuk kalimat
+     lebih dari satu baris paham ya ?? */
+
+
+/* di dalam program php bisa langsung menghitung angka tidak perlu membuat variabel terlebih
+dahulu dan menampilakan hasilnya ke layar*/ 
+ echo "<br> operator penjumlahan : 4+6 ="; echo 4+6;
+ echo "<br> opertaor pengurangan : 18-4 ="; echo 18-4;
+ echo "<br> operator perkalian   : 23* 3 ="; echo 23*3;
+ echo "<br> opertor pembagian    : 14/4 = "; echo 14/4;
+ echo "<br MODULUS : 15 MOD 4 ="; echo 15 % 4;
+?>
